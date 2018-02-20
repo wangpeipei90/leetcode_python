183335687bf45fdf2a09f29d4dcb23f890351834
peipei <pwang7@ncsu.edu>
2018-02-19 23:39:20 -0500

    658 find k closest elements

---
+++ 658.py
@@ -0,0 +1,80 @@
+'''
+Created on Feb 19, 2018
+
+@author: peipei
+'''
+import unittest
+from test.regrtest import runtest
+class Test658(unittest.TestCase):
+    def runTest(self):
+        self.failUnlessEqual(findClosestElements([1,2,3,4,5],4,3),[1,2,3,4],"fail 1")
+        self.failUnlessEqual(findClosestElements([1,2,3,4,5],4,-1),[1,2,3,4],"fail 2")
+
+def suite():
+    suite=unittest.TestSuite()
+    suite.addTest(Test658())
+    return suite
+
+# class Solution(object):        
+#     def findClosestElements(self, arr, k, x):
+def findClosestElements(arr, k, x):
+    """
+    :type arr: List[int]
+    :type k: int
+    :type x: int
+    :rtype: List[int]
+    """
+    n=len(arr)
+    p,q=0,n-1
+    l,r=0,n-1
+    while l<=r:        
+        mid=(l+r)//2
+        if arr[mid]==x:
+            p=q=mid
+            break
+        elif arr[mid]<x:
+            l=mid+1
+            p=mid
+        else:
+            r=mid-1
+            q=mid        
+    print(p,q)
+#     count=0
+    if p==q==0:
+        return arr[:k]
+    elif p==q==n-1:
+        return arr[-k:]
+    elif p==q:
+        p,q,count=p-1,q+1,1 
+    else:
+        count=0
+    print("count: ",count)
+    while count<k:
+        if p>=0 and q<=n-1:
+            if x-arr[p]<=arr[q]-x:
+                p-=1
+            else:
+                q+=1
+        elif p<0:
+            q+=1
+        elif q>n-1:
+            p-=1
+        count+=1
+    print(p,q)
+#     print("count: ",count)
+    p,q=max(p+1,0),min(q-1,n-1)
+    return arr[p:q+1]
+               
+if __name__ == '__main__':
+#     runner=unittest.TextTestRunner()
+#     runner.run(suite())
+#     res=findClosestElements([1,2,3,4,5],4,-1)
+#     print(res)
+#     res=findClosestElements([1,2,3,4,5],4,6)
+#     print(res)
+#     res=findClosestElements([1,2,3,4,5],4,3)
+#     print(res)
+#     res=findClosestElements([1,2,2,4,5],4,3)
+#     print(res)
+    res=findClosestElements([1,2,3,3,6,6,7,7,9,9],8,8)
+    print(res)
